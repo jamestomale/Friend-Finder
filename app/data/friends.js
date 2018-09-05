50de7e11ff7370a408c3e0b764757a250bf81a7c
unknown <jamestomale@gmail.com>
2018-09-05 17:54:05 -0400

    init

---
+++ app/data/friends.js
@@ -0,0 +1,65 @@
+var friendsData = [
+    {
+    "name":"Luke Skywalker",
+    "photo":"https://lumiere-a.akamaihd.net/v1/images/file_2729e1ec.jpeg?width=1200&region=0%2C0%2C2000%2C2000&quality=8",
+    "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+
+      "name":"Joe from Blue's Clues",
+      "photo":"https://i.ytimg.com/vi/y8_fY7zsXs8/maxresdefault.jpg",
+      "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+  
+      "name":"Queen Amidala",
+      "photo":"https://vignette.wikia.nocookie.net/starwars/images/4/41/Queenamidala.jpg/revision/latest?cb=20071103231504",
+      "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+
+      "name":"Queen Bee-yonce",
+      "photo":"https://cdn.skim.gs/image/upload/v1456338812/msi/beyonce-diapers_ybjgbf.jpg",
+      "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+
+    ]
+
+    }
+
+
+]  
+      
