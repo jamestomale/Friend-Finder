2fef2a428c825f3e45bc46584a262f7194c33ece
unknown <jamestomale@gmail.com>
2018-10-21 14:30:06 -0400

    data for friends complete

--- app/data/friends.js
+++ app/data/friends.js
@@ -17,7 +17,16 @@ var friendsData = [
     "name":"Joe from Blue's Clues",
     "photo":"https://i.ytimg.com/vi/y8_fY7zsXs8/maxresdefault.jpg",
     "scores":[
-]
+      2,
+      1,
+      1,
+      5,
+      5,
+      2,
+      2,
+      2,
+      4,
+      1]
       }, {
     "name":"Queen Amidala",
     "photo":"https://vignette.wikia.nocookie.net/starwars/images/4/41/Queenamidala.jpg/revision/latest?cb=20071103231504",
@@ -36,22 +45,47 @@ var friendsData = [
     "name":"Queen Bee-yonce",
     "photo":"https://cdn.skim.gs/image/upload/v1456338812/msi/beyonce-diapers_ybjgbf.jpg",
     "scores":[
-]
+      5,
+      5,
+      3,
+      3,
+      5,
+      4,
+      4,
+      1,
+      1,
+      3]
       }, {
     "name":"Jimmy Fallon",
-    "photo":"https://s.hdnux.com/photos/75/30/73/16092900/3/920x920.jpg"
+    "photo":"https://s.hdnux.com/photos/75/30/73/16092900/3/920x920.jpg",
     "scores":[
-
-         ]
+      3,
+      2,
+      5,
+      3,
+      5,
+      5,
+      4,
+      4,
+      5,
+      5]
       }, {    
     "name":"Cardi B",
-    "photo":"https://s.hdnux.com/photos/75/30/73/16092900/3/920x920.jpghttp://s3.amazonaws.com/hiphopdx-production/2017/12/Cardi-B-2-827x620.jpg"
+    "photo":"https://s.hdnux.com/photos/75/30/73/16092900/3/920x920.jpghttp://s3.amazonaws.com/hiphopdx-production/2017/12/Cardi-B-2-827x620.jpg",
     "scores":[
-
-         ]
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      1,
+      1,
+      3,
+      5]
       }, {    
     "name":"Donald Trump",
-    "photo":"https://poetsandquants.com/wp-content/uploads/2016/07/nbc-fires-donald-trump-after-he-calls-mexicans-rapists-and-drug-runners.jpg"
+    "photo":"https://poetsandquants.com/wp-content/uploads/2016/07/nbc-fires-donald-trump-after-he-calls-mexicans-rapists-and-drug-runners.jpg",
     "scores":[
       4,
       4,
@@ -63,10 +97,9 @@ var friendsData = [
       1,
       5,
       4]
-         ]
       }, {    
     "name":"Darth Vader",
-    "photo":"https://static1.srcdn.com/wordpress/wp-content/uploads/Darth-Vader-Rogue-One-details.jpg"
+    "photo":"https://static1.srcdn.com/wordpress/wp-content/uploads/Darth-Vader-Rogue-One-details.jpg",
     "scores":[
       4,
       2,
@@ -79,8 +112,8 @@ var friendsData = [
       4,
       3]
       }, { 
-        "name":"Batman",
-    "photo":"https://static1.squarespace.com/static/51b3dc8ee4b051b96ceb10de/51ce6099e4b0d911b4489b79/595cfa746b4998ce1a524392/1499270752252/director-matt-reeves-says-the-batman-will-be-an-emotional-story-and-he-explains-how-christopher-nolan-inspired-him-social.jpg"
+    "name":"Batman",
+    "photo":"https://static1.squarespace.com/static/51b3dc8ee4b051b96ceb10de/51ce6099e4b0d911b4489b79/595cfa746b4998ce1a524392/1499270752252/director-matt-reeves-says-the-batman-will-be-an-emotional-story-and-he-explains-how-christopher-nolan-inspired-him-social.jpg",
     "scores":[
       5,
       2,
@@ -92,10 +125,9 @@ var friendsData = [
       4,
       1,
       2]
-         ]
       }, {    
     "name":"Deadpool",
-    "photo":"https://img1.looper.com/img/gallery/what-you-need-to-know-before-you-see-deadpool-2/intro-1525691373.jpg"
+    "photo":"https://img1.looper.com/img/gallery/what-you-need-to-know-before-you-see-deadpool-2/intro-1525691373.jpg",
     "scores":[
       1,
       3,
@@ -107,7 +139,6 @@ var friendsData = [
       4,
       4,
       5]
-         ]
       }];
       
 module.exports = friends;      
