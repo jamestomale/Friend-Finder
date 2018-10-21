eb95d08a4c04dd1f6d5a6aeaed3bfb2380537889
unknown <jamestomale@gmail.com>
2018-10-20 21:00:05 -0400

    more date of how compatible and added survey question/statements

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,15 +4,15 @@ var friendsData = [
     "photo":"https://lumiere-a.akamaihd.net/v1/images/file_2729e1ec.jpeg?width=1200&region=0%2C0%2C2000%2C2000&quality=8",
     "scores":[
       5,
-      1,
+      3,
       4,
       4,
       5,
       1,
       2,
-      5,
-      4,
-      1]
+      1,
+      1,
+      3]
      }, {
     "name":"Joe from Blue's Clues",
     "photo":"https://i.ytimg.com/vi/y8_fY7zsXs8/maxresdefault.jpg",
@@ -22,7 +22,16 @@ var friendsData = [
     "name":"Queen Amidala",
     "photo":"https://vignette.wikia.nocookie.net/starwars/images/4/41/Queenamidala.jpg/revision/latest?cb=20071103231504",
     "scores":[
-]
+      5,
+      5,
+      3,
+      3,
+      5,
+      4,
+      4,
+      1,
+      1,
+      3]
       }, {
     "name":"Queen Bee-yonce",
     "photo":"https://cdn.skim.gs/image/upload/v1456338812/msi/beyonce-diapers_ybjgbf.jpg",
@@ -44,25 +53,60 @@ var friendsData = [
     "name":"Donald Trump",
     "photo":"https://poetsandquants.com/wp-content/uploads/2016/07/nbc-fires-donald-trump-after-he-calls-mexicans-rapists-and-drug-runners.jpg"
     "scores":[
-
+      4,
+      4,
+      3,
+      2,
+      2,
+      2,
+      1,
+      1,
+      5,
+      4]
          ]
       }, {    
     "name":"Darth Vader",
     "photo":"https://static1.srcdn.com/wordpress/wp-content/uploads/Darth-Vader-Rogue-One-details.jpg"
     "scores":[
-
-         ]
+      4,
+      2,
+      5,
+      3,
+      2,
+      4,
+      4,
+      4,
+      4,
+      3]
       }, { 
         "name":"Batman",
     "photo":"https://static1.squarespace.com/static/51b3dc8ee4b051b96ceb10de/51ce6099e4b0d911b4489b79/595cfa746b4998ce1a524392/1499270752252/director-matt-reeves-says-the-batman-will-be-an-emotional-story-and-he-explains-how-christopher-nolan-inspired-him-social.jpg"
     "scores":[
-
+      5,
+      2,
+      1,
+      2,
+      2,
+      2,
+      1,
+      4,
+      1,
+      2]
          ]
       }, {    
     "name":"Deadpool",
     "photo":"https://img1.looper.com/img/gallery/what-you-need-to-know-before-you-see-deadpool-2/intro-1525691373.jpg"
     "scores":[
-
+      1,
+      3,
+      2,
+      5,
+      1,
+      2,
+      1,
+      4,
+      4,
+      5]
          ]
       }];
       
